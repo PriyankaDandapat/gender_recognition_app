a3e9201f2015663ce6d29478b060daf5623b5813
PriyankaDandapat <144220616+PriyankaDandapat@users.noreply.github.com>
2024-04-28 17:18:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,3 @@ app=Flask(__name__)   #webserver gateway interphase (WSGI)
 app.add_url_rule(rule='/',endpoint='home',view_func=veiws.index)
 app.add_url_rule(rule='/app/',endpoint='app',view_func=veiws.app)
 app.add_url_rule(rule='/app/gender/',endpoint='gender',view_func=veiws.genderapp, methods=['GET','POST'])
-
-if __name__=="__main__":
-    app.run(debug=True)
