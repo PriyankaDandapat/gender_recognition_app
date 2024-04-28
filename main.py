14048accbbee2643d86a97f1a96a30185a99f31c
PriyankaDandapat <priyankadandapat8@gmail.com>
2024-04-28 16:31:26 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from app import veiws
+
+app=Flask(__name__)   #webserver gateway interphase (WSGI)
+
+app.add_url_rule(rule='/',endpoint='home',view_func=veiws.index)
+app.add_url_rule(rule='/app/',endpoint='app',view_func=veiws.app)
+app.add_url_rule(rule='/app/gender/',endpoint='gender',view_func=veiws.genderapp, methods=['GET','POST'])
+
+if __name__=="__main__":
+    app.run(debug=True)
